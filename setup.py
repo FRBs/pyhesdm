4988384c89bb0685bd09d256784407e47e1cfe27
Yuxin Huang <mochafhxy@gmail.com>
2025-05-12 13:14:51 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ class PostInstallCommand(install):
 
 setup(
     name='pyhesdm',
-    version='0.1',
+    version='0.1.1',
     description='Local Universe Dispersion Measure Model Computed from HESTIA Simulation',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
